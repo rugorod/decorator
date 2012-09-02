060f54458bbfd9042c2d79fdbb5a31d810435eb4
Taras <tapacpro@gmail.com>
2012-09-02 20:46:48 +0600

    new repository for decorator-template main branch in taras@rugorod.com:/home/taras/git/decorator/

---
+++ static/js/vegas.config-home.js
@@ -0,0 +1,12 @@
+$( function() {
+    $.vegas('slideshow', {
+	backgrounds:[
+	    { src:'/img/slider/bg1.jpg', fade:1000 },
+	    { src:'/img/slider/bg2.jpg', fade:1000 },
+	    { src:'/img/slider/bg3.jpg', fade:1000 },
+	    { src:'/img/slider/bg4.jpg', fade:1000 }
+	]
+    })('overlay', {
+	src:'/img/overlays/11.png'
+    });
+});
